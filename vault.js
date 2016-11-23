c915255346a4c19bb5e1685312ca0469c6b5649f
Aukai Tirrell <mtirrell@hawaii.edu>
2016-11-22 22:55:16 -1000

    got secret vault to work

--- vault.js
+++ vault.js
@@ -1,4 +1,23 @@
 'use strict';
 module.exports = function() {
-  
+
+  var secretVault = {};
+
+  function _setValue(key,value) {
+    secretVault[key] = value;
+
+  }
+
+  function _getValue(key) {
+    if(secretVault.hasOwnProperty(key)) {
+      return secretVault[key];
+    } else {
+      return null;
+    }
+  }
+
+  return {
+    setValue: _setValue,
+    getValue: _getValue
+  };
 };
